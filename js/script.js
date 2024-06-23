53c3dd7b439012b5e0286419393082f5e1908604
Manish Mali <manishmali448278@gmail.com>
2024-06-24 00:55:05 +0530

    Build New Section and updates Old Section

--- js/script.js
+++ js/script.js
@@ -1,38 +1,19 @@
 $(document).ready(function () {
-    // Hero Section Slider
-
-
-    // Service Section Slider
-    $('.owl-theme').owlCarousel({
-        loop: true,
-        margin: 10,
-        nav: true,
-        // autoplay: true,
-        // autoplayTimeout: 2000,
-
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 3
-            },
-            1000: {
-                items: 3
-            }
-        }
-    })
-
+    $(".tabs__button a").click(function() {
+        $(".tabs__button a").removeClass('activetab');
+        $(this).addClass('activetab');
+        var ids = $(this).attr('href');
     
 
-    $(".tabs__button a").click(function(){
-        $(".tabs__button a").removeClass('active')
-        $(this).addClass('active')
-        var ids = $(this).attr('href')
-        $('.tech__head').hide();
-        $(ids).show();
+        $('.tech__head:visible').fadeOut(200, function() {
+
+            $(ids).fadeIn(200);
+        });
+    
         return false;
-    })
+    });
 });
 
  
+
+
